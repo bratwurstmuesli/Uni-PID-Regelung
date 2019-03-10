733c18d3536f2c0667a80abe8466417ed6287629
bratwurstmuesli <deamondoener@googlemail.com>
2019-03-10 16:03:05 +0100

    dd

--- ESPRegelung/ESPRegelung/ESPRegelung.ino
+++ ESPRegelung/ESPRegelung/ESPRegelung.ino
@@ -16,6 +16,8 @@ const int ledChannel = 0;
 const int ledChannel1 = 1;
 const int resolution = 8;
 
+//speedpot
+const int speedpotpin = 34;
 
 //Stromstärke-Messung
 //ACS712-5
@@ -50,6 +52,8 @@ void speedcontrol(int pwm, char a) {
 		ledcWrite(ledChannel, 0);
 		ledcWrite(ledChannel1, 0);
 	}
+	//Serial.print("PWM: ");
+	//Serial.println(pwm);
 }
 
 //ON ICE
@@ -67,12 +71,21 @@ const byte interruptPin = 33;
 volatile int interruptCounter = 0;
 int numberOfInterrupts = 0;
 
+const byte interruptPinhigh = 32;
+volatile int interruptCounterhigh = 0;
+int numberOfInterruptshigh = 0;
+
 portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
 
 void IRAM_ATTR handleInterrupt() {
 	portENTER_CRITICAL_ISR(&mux);
 	interruptCounter++;
-	countpulse();
+	portEXIT_CRITICAL_ISR(&mux);
+}
+
+void handleInterrupthigh() {
+	portENTER_CRITICAL_ISR(&mux);
+	interruptCounterhigh++;
 	portEXIT_CRITICAL_ISR(&mux);
 }
 
@@ -81,29 +94,41 @@ void countpulse() {
 	//Serial.print("t2: ");
 	//Serial.println(t2);
 	if (t2 != 0) {
-		unsigned long t1 = micros();
+		unsigned long t1 = millis();
 		//Serial.print("t1: ");
 		//Serial.println(t1);
-		unsigned long zeitdiff = t1 - t2;
-		if (zeitdiff != 0) {
-			//Serial.print("zeitdiff: ");
-			//Serial.println(zeitdiff);
-			rpm = 1*1000*1000 / zeitdiff; //sekunden*millisekunden*mikrosekunde
-			//Serial.print("RPM: ");
+		if (t2 < t1 && numberOfInterrupts != 0) {
+			unsigned long zeitdiff = t1 - t2;
+			//Serial.print("numberOfInterrupts: ");
+			//Serial.println(numberOfInterrupts);
+			int mittelwertrotatation = zeitdiff / numberOfInterrupts;
+			//int rpm = (60 * 1000) / mittelwertrotatation;
+			//Serial.print("rpm: ");
 			//Serial.println(rpm);
-			t2 = micros();
+			numberOfInterrupts = 0;
 		}
 	}
-	t2 = micros();
+	t2 = millis();
 }
+
+void setspeed() {
+	int analogvalue = analogRead(speedpotpin);
+	int mapvalue = map(analogvalue, 0, 4096, 0, 255);
+	speedcontrol(mapvalue, 'f');
+	//Serial.println(analogvalue);
+}
+
 
 void setup() {
 	Serial.begin(115200);
 	Serial.println("TEST");
 
-	//count pulse
 	pinMode(interruptPin, INPUT);
-	attachInterrupt(digitalPinToInterrupt(interruptPin), handleInterrupt, FALLING);
+	pinMode(interruptPinhigh, INPUT);
+	attachInterrupt(digitalPinToInterrupt(interruptPin), handleInterrupt, LOW); //falling normally
+	attachInterrupt(digitalPinToInterrupt(interruptPinhigh), handleInterrupthigh, HIGH);
+
+
 
 	//PWM einbauen
 	//https://randomnerdtutorials.com/esp32-pwm-arduino-ide/
@@ -116,16 +141,19 @@ void setup() {
 	pinMode(speedpin2, OUTPUT);
 
 	//provisorisch
-	speedcontrol(170, 'f');
+	//speedcontrol(180, 'f');
 }
 
 void loop() {
-	/*static unsigned long previousMillis = 0;
+	static unsigned long previousMillis = 0;
 	unsigned long currentMillis = millis();
 	const long interval = 100;
 	if (currentMillis - previousMillis >= interval) {
 		previousMillis = currentMillis;
-		static int fadevalue = 100;
+
+		setspeed();
+
+		/*static int fadevalue = 100;
 		static int fade = 1;
 		if (fadevalue <= 255 && fade == 1) {
 			fadevalue = fadevalue + 5;
@@ -139,27 +167,53 @@ void loop() {
 				fade = 1;
 			}
 		}
-		speedcontrol(fadevalue, 'f');
-	}*/
+		speedcontrol(fadevalue, 'f');*/
+	}
 
 	static unsigned long previousMillis1 = 0;
 	unsigned long currentMillis1 = millis();
 	const long interval1 = 500;
 	if (currentMillis1 - previousMillis1 >= interval1) {
 		previousMillis1 = currentMillis1;
-		Serial.println(rpm);
+		//countpulse();
+		//Serial.println(rpm);
 	}
 
-	if (interruptCounter > 0) {
 
+	if (interruptCounter > 0) {
+		Serial.println(interruptCounter);
 		portENTER_CRITICAL(&mux);
 		interruptCounter--;
 		portEXIT_CRITICAL(&mux);
 
-		numberOfInterrupts++;
-		//Serial.print("An interrupt has occurred. Total: ");
-		//Serial.println(numberOfInterrupts);
-		//Serial.print("RPM: ");
-		//Serial.println(countpulse());
+		static unsigned long previousMillis2 = 0;
+		unsigned long currentMillis2 = millis();
+		const long interval2 = 15;
+		if (currentMillis2 - previousMillis2 >= interval2) {
+			previousMillis2 = currentMillis2;
+
+			numberOfInterrupts++;
+			Serial.print("LOWK: ");
+			Serial.println(numberOfInterrupts);
+
+		}
+	}
+	if (interruptCounterhigh > 0) {
+		Serial.println(interruptCounterhigh);
+		portENTER_CRITICAL(&mux);
+		interruptCounterhigh--;
+		portEXIT_CRITICAL(&mux);
+
+		static unsigned long previousMillis3 = 0;
+		unsigned long currentMillis3 = millis();
+		const long interval3 = 15;
+		if (currentMillis3 - previousMillis3 >= interval3) {
+			previousMillis3 = currentMillis3;
+
+			numberOfInterruptshigh++;
+			Serial.print("HIGH: ");
+			Serial.println(numberOfInterruptshigh);
+
+		}
 	}
 }
